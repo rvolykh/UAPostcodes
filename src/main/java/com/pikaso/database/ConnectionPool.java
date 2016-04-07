2a76df117f283e94a2c2518be927a4050478ef16
rvolykh <b.pomka@gmail.com>
2016-04-07 11:44:44 +0300

    dont work

--- src/main/java/com/pikaso/database/ConnectionPool.java
+++ src/main/java/com/pikaso/database/ConnectionPool.java
@@ -1,6 +1,5 @@
 package com.pikaso.database;
 
-import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
@@ -10,24 +9,23 @@ import org.slf4j.LoggerFactory;
 
 import com.pikaso.constants.Constants;
 
+
 public final class ConnectionPool {
     private static final Logger LOGGER = LoggerFactory.getLogger(ConnectionPool.class);
     
     @SuppressWarnings("unused")
     private static final ConnectionPool connectionPool = new ConnectionPool();
     
-    private static DataSource dataSource;
-
+    private static DataSource dataSource;// = methd();
+    
     private ConnectionPool() {
         try {
-            InitialContext ic = new InitialContext();
-            Context initialContext = (Context) ic.lookup("java:comp/env");
-            dataSource = (DataSource) initialContext.lookup("jdbc/MySQLDS");
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/MySQLDS");
         } catch (NamingException e) {
             try {
                 dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/pool-ds");
             } catch (NamingException e1) {
-                LOGGER.error("Can't establish connection with Database",e1);
+                LOGGER.error("Can't establish connection with Database",e);
             }
         }
     }
